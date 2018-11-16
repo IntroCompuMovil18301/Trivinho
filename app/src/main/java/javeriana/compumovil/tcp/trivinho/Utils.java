8f701e8f2ba0915525d610ff9a595b63ba602eb5
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2018-11-16 12:51:51 -0500

    Update Utils.java

--- app/src/main/java/javeriana/compumovil/tcp/trivinho/Utils.java
+++ app/src/main/java/javeriana/compumovil/tcp/trivinho/Utils.java
@@ -6,6 +6,8 @@ public class Utils {
     static final String PATH_HUESPEDES = "huespedes/";
     static final String PATH_ANFITRIONES = "anfitriones/";
     private static String PATH_FECHAS = "fechasdisponibles/";
+    
+    
     private static String PATH_ALOJAMIENTOS = "alojamientos/";
     private static String PATH_FOTOSALOJAMIENTO = "fotosalojamientos/";
 
