b194a4e79a78726c4fe9ec91f423f2a1969bc6af
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2018-11-17 22:38:41 -0500

    RUTAS V5

--- Trivinho/app/src/main/java/javeriana/compumovil/tcp/trivinho/ReservarAlojamiento.java
+++ Trivinho/app/src/main/java/javeriana/compumovil/tcp/trivinho/ReservarAlojamiento.java
@@ -414,4 +414,50 @@ public class ReservarAlojamiento extends FragmentActivity implements OnMapReadyC
     }
 
 
+    public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+        //Este método PARSEA el JSONObject que retorna del API de Rutas de Google devolviendo
+        //una lista del lista de HashMap Strings con el listado de Coordenadas de Lat y Long,
+        //con la cual se podrá dibujar pollinas que describan la ruta entre 2 puntos.
+        JSONArray jRoutes = null;
+        JSONArray jLegs = null;
+        JSONArray jSteps = null;
+
+        try {
+
+            jRoutes = jObject.getJSONArray("routes");
+
+            /** Traversing all routes */
+            for(int i=0;i<jRoutes.length();i++){
+                jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+                /** Traversing all legs */
+                for(int j=0;j<jLegs.length();j++){
+                    jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                    /** Traversing all steps */
+                    for(int k=0;k<jSteps.length();k++){
+                        String polyline = "";
+                        polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                        List<LatLng> list = decodePoly(polyline);
+
+                        /** Traversing all points */
+                        for(int l=0;l<list.size();l++){
+                            HashMap<String, String> hm = new HashMap<String, String>();
+                            hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                            hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+                            path.add(hm);
+                        }
+                    }
+                    routes.add(path);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+        }
+        return routes;
+    }
+
+
 }
