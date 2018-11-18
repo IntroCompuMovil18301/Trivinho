30ce098284469c1a8ce0cb46be204d6499087302
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2018-11-17 21:33:42 -0500

    Revert "Revert "Revert "Reservar V1"""
    This reverts commit 70fd5c3048b11cdd486b7578986030f843d72cf4.

--- Trivinho/app/src/main/java/javeriana/compumovil/tcp/trivinho/ReservarAlojamiento.java
+++ Trivinho/app/src/main/java/javeriana/compumovil/tcp/trivinho/ReservarAlojamiento.java
@@ -1,85 +1,21 @@
 package javeriana.compumovil.tcp.trivinho;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.DialogFragment;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javeriana.compumovil.tcp.trivinho.negocio.Alojamiento;
-
-public class ReservarAlojamiento extends FragmentActivity implements OnMapReadyCallback  {
+public class ReservarAlojamiento extends AppCompatActivity {
 
     private EditText mfechaInicio;
     private EditText mfechaFinal;
 
-    public static List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>();
-
-    private LocationRequest mLocationRequest;
-    private LocationCallback mLocationCallback;
-    private FusedLocationProviderClient mFusedLocationClient;
-
     private GoogleMap mMap;
-    private double latitudActual;
-    private double longitudActual;
-
-    private Alojamiento alojamiento;
-    private static final int ID_PERMISSION_LOCATION = 1;
-    private static final int REQUEST_CHECK_SETTINGS = 1;
-
-    private static boolean ubicacionInicialColocada = false;
-
-    JsonObjectRequest jsonObjectRequest;
-    RequestQueue request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,38 +23,8 @@ public class ReservarAlojamiento extends FragmentActivity implements OnMapReadyC
         setContentView(R.layout.activity_reservar_alojamiento);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapp3);
-        mapFragment.getMapAsync(this);
-
-        mfechaInicio = (EditText) findViewById(R.id.fechaInicio3);
-        mfechaFinal = (EditText) findViewById(R.id.fechaFinal3);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mLocationRequest = createLocationRequest();
-
-        alojamiento = (Alojamiento) getIntent().getSerializableExtra("alojamiento");
-
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                Location location = locationResult.getLastLocation();
-                Log.i("LOCATION", "Location update in the callback: " + location);
-                if (location != null) {
-                    latitudActual = location.getLatitude();
-                    longitudActual = location.getLongitude();
-                    Log.i ("Coordenada Actuales", latitudActual+" "+longitudActual);
-
-                    if (!ubicacionInicialColocada && mMap!=null) {
-                        webServiceObtenerRuta(latitudActual, longitudActual, alojamiento.getLatitud(), alojamiento.getLongitud());
-                        ubicarMapaPosicion(latitudActual, longitudActual);
-                        ubicacionInicialColocada = true;
-                        colocarAlojamiento(alojamiento);
-
-                    }
-
-                }
-            }
-        };
+                .findFragmentById(R.id.map3);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
 
 
         mfechaInicio.setOnClickListener(new View.OnClickListener() {
@@ -139,272 +45,15 @@ public class ReservarAlojamiento extends FragmentActivity implements OnMapReadyC
             }
         });
 
-
-
     }
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Log.i("ENTRO: ", "MAPA MOSTRADO");
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
-    }
 
-
-    protected LocationRequest createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000); //tasa de refresco en milisegundos
-        mLocationRequest.setFastestInterval(5000); //máxima tasa de refresco
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return mLocationRequest;
-    }
-
-    private void requestPermission(Activity context, String permission, String explanation, int requestId) {
-        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-            // Should we show an explanation?Â  Â
-            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
-                Toast.makeText(context, explanation, Toast.LENGTH_LONG).show();
-            }
-            ActivityCompat.requestPermissions(context, new String[]{permission}, requestId);
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case ID_PERMISSION_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Ya hay permiso para acceder a la localización", Toast.LENGTH_LONG).show();
-                    startLocationUpdates();
-                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
-                    SettingsClient client = LocationServices.getSettingsClient(this);
-                    Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-
-                    task.addOnFailureListener(this, new OnFailureListener() {
-                        public void onFailure(@NonNull Exception e) {
-                            Log.i("Ubicación desactivada", "si");
-                            int statusCode = ((ApiException) e).getStatusCode();
-                            switch (statusCode) {
-                                case CommonStatusCodes.RESOLUTION_REQUIRED:
-// Location settings are not satisfied, but this can be fixed by showing the user a dialog.
-                                    try {// Show the dialog by calling startResolutionForResult(), and check the result in onActivityResult().
-                                        ResolvableApiException resolvable = (ResolvableApiException) e;
-                                        resolvable.startResolutionForResult(ReservarAlojamiento.this,
-                                                REQUEST_CHECK_SETTINGS);
-                                    } catch (IntentSender.SendIntentException sendEx) {
-// Ignore the error.
-                                    }
-                                    break;
-                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-// Location settings are not satisfied. No way to fix the settings so we won't show the dialog.
-                                    break;
-                            }
-                        }
-                    });
-
-                } else {
-                    Toast.makeText(this, "No hay Permiso", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-
-        }
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_CHECK_SETTINGS: {
-                if (resultCode == RESULT_OK) {
-                    startLocationUpdates(); //Se encendió la localización!!!
-                } else {
-                    Toast.makeText(this,
-                            "Sin acceso a localización, hardware deshabilitado!",
-                            Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-        }
-    }
-
-    protected void onResume() {
-        super.onResume();
-        startLocationUpdates();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-
-    private void startLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-                    mLocationCallback, null);
-        }
-    }
-
-    private void stopLocationUpdates() {
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-    }
-
-    private void ubicarMapaPosicion(double latitud, double longitud) {
-        LatLng ubicacion = new LatLng(latitud, longitud);
-        mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(ubicacion).title("Ubicación central")
-                .icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
-    }
-
-    private void webServiceObtenerRuta(Double latitudInicial, Double longitudInicial, Double latitudFinal, Double longitudFinal) {
-
-        String url="https://maps.googleapis.com/maps/api/directions/json?origin="+String.valueOf(latitudInicial)+
-                ","+String.valueOf(longitudInicial)+"&destination="+String.valueOf(latitudFinal)+","+
-                String.valueOf(longitudFinal)+"&key=AIzaSyCU64UC6DO5Ph9fm0Ldif8pA5MaeyhGq7Y";
-
-        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                //Este método PARSEA el JSONObject que retorna del API de Rutas de Google devolviendo
-                //una lista del lista de HashMap Strings con el listado de Coordenadas de Lat y Long,
-                //con la cual se podrá dibujar pollinas que describan la ruta entre 2 puntos.
-                JSONArray jRoutes = null;
-                JSONArray jLegs = null;
-                JSONArray jSteps = null;
-
-                try {
-
-                    jRoutes = response.getJSONArray("routes");
-
-                    /** Traversing all routes */
-                    for(int i=0;i<jRoutes.length();i++){
-                        jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
-                        List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
-
-                        /** Traversing all legs */
-                        for(int j=0;j<jLegs.length();j++){
-                            jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
-
-                            /** Traversing all steps */
-                            for(int k=0;k<jSteps.length();k++){
-                                String polyline = "";
-                                polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
-                                List<LatLng> list = decodePoly(polyline);
-
-                                /** Traversing all points */
-                                for(int l=0;l<list.size();l++){
-                                    HashMap<String, String> hm = new HashMap<String, String>();
-                                    hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
-                                    hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
-                                    path.add(hm);
-                                }
-                            }
-                            routes.add(path);
-                        }
-                        dibujarRuta();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }catch (Exception e){
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "No se puede conectar "+error.toString(), Toast.LENGTH_LONG).show();
-                System.out.println();
-                Log.d("ERROR: ", error.toString());
-            }
-        }
-        );
-
-        request.add(jsonObjectRequest);
-    }
-
-    private List<LatLng> decodePoly(String encoded) {
-
-        List<LatLng> poly = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
-
-        while (index < len) {
-            int b, shift = 0, result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lat += dlat;
-
-            shift = 0;
-            result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lng += dlng;
-
-            LatLng p = new LatLng((((double) lat / 1E5)),
-                    (((double) lng / 1E5)));
-            poly.add(p);
-        }
-
-        return poly;
-    }
-
-    private void dibujarRuta(){
-        LatLng center = null;
-        ArrayList<LatLng> points = null;
-        PolylineOptions lineOptions = null;
-
-        // setUpMapIfNeeded();
-
-        // recorriendo todas las rutas
-        for(int i=0;i<routes.size();i++){
-            points = new ArrayList<LatLng>();
-            lineOptions = new PolylineOptions();
-
-            // Obteniendo el detalle de la ruta
-            List<HashMap<String, String>> path = routes.get(i);
-
-            // Obteniendo todos los puntos y/o coordenadas de la ruta
-            for(int j=0;j<path.size();j++){
-                HashMap<String,String> point = path.get(j);
-
-                double lat = Double.parseDouble(point.get("lat"));
-                double lng = Double.parseDouble(point.get("lng"));
-                LatLng position = new LatLng(lat, lng);
-
-                if (center == null) {
-                    //Obtengo la 1ra coordenada para centrar el mapa en la misma.
-                    center = new LatLng(lat, lng);
-                }
-                points.add(position);
-            }
-
-            // Agregamos todos los puntos en la ruta al objeto LineOptions
-            lineOptions.addAll(points);
-            //Definimos el grosor de las Polilíneas
-            lineOptions.width(2);
-            //Definimos el color de la Polilíneas
-            lineOptions.color(Color.GREEN);
-        }
-
-    }
-
-    private void colocarAlojamiento (Alojamiento alojamiento){
-        LatLng ubicacion = new LatLng(alojamiento.getLatitud(), alojamiento.getLongitud());
-        Marker amarker = mMap.addMarker(new MarkerOptions().position(ubicacion).title("Alojamiento")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marcadorcasa)));
     }
 
 
